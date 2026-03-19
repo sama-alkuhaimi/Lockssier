f3525444153ebd81137f20314bdb79fc8fcad5e8
sama-alkuhaimi <alkuhaimisama@gmail.com>
2026-03-19 09:47:46 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'lockssier-v1';
+const ASSETS = [
+  '/',
+  '/index.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
